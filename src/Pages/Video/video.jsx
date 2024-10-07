f3e7ebad1d86c52f2c0c923247f2e89e25d3f6c7
Shahbaz Ali <aquibahmad345@gmail.com>
2024-10-07 20:08:52 +0530

    add static video component to video page

--- src/Pages/Video/video.jsx
+++ src/Pages/Video/video.jsx
@@ -10,12 +10,60 @@ import thumbnail1 from "../../assets/thumbnail1.png";
 // import thumbnail6 from "../../assets/thumbnail6.png";
 // import thumbnail7 from "../../assets/thumbnail7.png";
 // import thumbnail8 from "../../assets/thumbnail8.png";
+import video from "../../assets/video.mp4";
+import like from "../../assets/like.png";
+import dislike from "../../assets/dislike.png";
+import share from "../../assets/share.png";
+import save from "../../assets/save.png";
+
+import jack from "../../assets/jack.png";
 
 function Video() {
   return (
     <div className={styles.playContainer}>
-      <div className={styles.playVideo}></div>
+      <PlayVideo />
       <Recommended />
+    </div>
+  );
+}
+
+function PlayVideo() {
+  return (
+    <div className={styles.playVideo}>
+      <video
+        src={video}
+        className={styles.video}
+        controls
+        autoPlay
+        muted
+      ></video>
+      <h3>Video title</h3>
+      <div className={styles.playVideoInfo}>
+        <p>15k views &bull; Just now</p>
+        <div>
+          <span>
+            <img src={like} alt="" /> 10
+          </span>
+          <span>
+            <img src={dislike} alt="" /> 5
+          </span>
+          <span>
+            <img src={share} alt="" /> Share
+          </span>
+          <span>
+            <img src={save} alt="" /> Save
+          </span>
+        </div>
+      </div>
+      <hr />
+      <div className={styles.publisher}>
+        <img src={jack} alt="" />
+        <div>
+          <p>Channel Name</p>
+          <span>12k Subscribers</span>
+        </div>
+        <button type="button">Subscribe</button>
+      </div>
     </div>
   );
 }
@@ -30,7 +78,7 @@ function Recommended() {
         <div className={styles.info}>
           <h4>Thumbnail1</h4>
           <p>ChannelName</p>
-          <p>15k views &bull; Just now</p>
+          <p className={styles.infoViews}>15k views </p>
         </div>
       </div>
       <div className={styles.sideVideo}>
@@ -40,7 +88,7 @@ function Recommended() {
         <div className={styles.info}>
           <h4>Thumbnail1</h4>
           <p>ChannelName</p>
-          <p>15k views &bull; Just now</p>
+          <p className={styles.infoViews}>15k views </p>
         </div>
       </div>
       <div className={styles.sideVideo}>
@@ -50,7 +98,7 @@ function Recommended() {
         <div className={styles.info}>
           <h4>Thumbnail1</h4>
           <p>ChannelName</p>
-          <p>15k views &bull; Just now</p>
+          <p className={styles.infoViews}>15k views </p>
         </div>
       </div>
       <div className={styles.sideVideo}>
@@ -60,7 +108,7 @@ function Recommended() {
         <div className={styles.info}>
           <h4>Thumbnail1</h4>
           <p>ChannelName</p>
-          <p>15k views &bull; Just now</p>
+          <p className={styles.infoViews}>15k views </p>
         </div>
       </div>
       <div className={styles.sideVideo}>
@@ -70,7 +118,7 @@ function Recommended() {
         <div className={styles.info}>
           <h4>Thumbnail1</h4>
           <p>ChannelName</p>
-          <p>15k views &bull; Just now</p>
+          <p className={styles.infoViews}>15k views </p>
         </div>
       </div>
       <div className={styles.sideVideo}>
@@ -80,7 +128,7 @@ function Recommended() {
         <div className={styles.info}>
           <h4>Thumbnail1</h4>
           <p>ChannelName</p>
-          <p>15k views &bull; Just now</p>
+          <p className={styles.infoViews}>15k views </p>
         </div>
       </div>
       <div className={styles.sideVideo}>
@@ -90,7 +138,7 @@ function Recommended() {
         <div className={styles.info}>
           <h4>Thumbnail1</h4>
           <p>ChannelName</p>
-          <p>15k views &bull; Just now</p>
+          <p className={styles.infoViews}>15k views </p>
         </div>
       </div>
       <div className={styles.sideVideo}>
@@ -100,7 +148,7 @@ function Recommended() {
         <div className={styles.info}>
           <h4>Thumbnail1</h4>
           <p>ChannelName</p>
-          <p>15k views &bull; Just now</p>
+          <p className={styles.infoViews}>15k views </p>
         </div>
       </div>
       <div className={styles.sideVideo}>
@@ -110,7 +158,7 @@ function Recommended() {
         <div className={styles.info}>
           <h4>Thumbnail1</h4>
           <p>ChannelName</p>
-          <p>15k views &bull; Just now</p>
+          <p className={styles.infoViews}>15k views </p>
         </div>
       </div>
     </div>
