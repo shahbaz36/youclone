a1052277afe73dd3d98c2428dafbf681530caddc
Shahbaz Ali <aquibahmad345@gmail.com>
2025-09-13 20:04:28 +0530

    Refactor: Explicitly add .jsx extension to component imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageNav from "./components/Navbar/PageNav";
 import { useState } from "react";
-import Home from "./Pages/Home/Home";
-import Video from "./Pages/Video/Video";
+import Home from "./Pages/Home/Home.jsx";
+import Video from "./Pages/Video/Video.jsx";
 
 function App() {
   const [isHidden, setIsHidden] = useState(false);
